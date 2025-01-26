4e6543cff9f03de6bcbb908543a5e182aa80bf5d
tslomczewski <tslomczewski@gmail.com>
2025-01-27 00:14:17 +0100

    added app layer in client project

---
+++ Client/ApplicationLayer/DatabaseStructureService.cs
@@ -0,0 +1,16 @@
+﻿using Shared.Models;
+
+namespace ApplicationLayer;
+
+public class DatabaseStructureService : IDatabaseStructureService
+{
+    public Task<DatabaseStructure> GetDatabaseStructureAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<TablesStructure> GetTablesStructureAsync()
+    {
+        throw new NotImplementedException();
+    }
+}
